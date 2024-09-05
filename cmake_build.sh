b084c7a25ea186c000a29f10b712ee47cbb60344
Angel Stojanov <stojanovangel@outlook.com>
2024-09-06 01:59:53 +0200

    Added platforms, cleaned up cmake and build scripts

--- cmake_build.sh
+++ cmake_build.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
+
+if [ $# -gt 0 ]; then
+    if echo "$1" | grep -qi "debug"; then
+        build_type="Debug"
+    elif echo "$1" | grep -qi "release"; then
+        build_type="Release"
+    elif [ "$1" == "--d" ]; then
+        build_type="Debug"
+    elif [ "$1" == "--r" ]; then
+        build_type="Release"
+    else
+        build_type="Debug"
+    fi
+else
+    build_type="Debug"
+fi
+
 if [ ! -d ./build ]; then
-    ./cmake_generate.sh
+    ./cmake_generate.sh "$build_type"
 fi 
 
 if cmake --build build -j $(nproc); then
